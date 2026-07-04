b268ed6e7f340cf48570d1f5ae59d9400bb5c12c
Anson K Santhosh <ansonsanthosh7@gmail.com>
2026-07-04 21:52:59 +0530

    added base classes and dictionary convesion

---
+++ core/case.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+class Case:
+    def __init__(self, case_id, investigator, description, evidence_path):
+        self.case_id = case_id
+        self.investigator = investigator
+        self.description = description
+        self.evidence_path = evidence_path
+        self.created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    #converting the case object to dictionary
+    #since custom write onto JSON not possible
+
+    def to_dict(self):                          
+            return {
+            "case_id":self.case_id,
+            "investigator":self.investigator,
+            "description":self.description,
+            "evidence_path":self.evidence_path,
+            "created_at":self.created_at,
+
+    }
